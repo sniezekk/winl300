ca3be6733fdac569ff1c3654772fd6464daff30b
Korey Sniezek <sniezekk@mymacewan.ca>
2021-11-21 19:58:15 -0700

    Initial commit

---
+++ src/main/kotlin/com/winl300/GraphQLDemo/GraphQlDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.winl300.GraphQLDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphQlDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphQlDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/winl300/GraphQLDemo/GraphQlDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.winl300.GraphQLDemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphQlDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
